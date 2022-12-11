ea6a63bee698fbed02b179dc22c805889583e69f
Ashish M J <ashm.jagadeesh@gmail.com>
2022-12-12 00:50:29 +0530

    Update service API

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM python:3.8
 WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
+CMD ["python","app.py"]
--- app.py
+++ app.py
@@ -4,11 +4,13 @@ app.config['SECRET_KEY']='XXXXXXX'
 
 from mailjet_rest import Client
 
-
-api_key = '5b0e93b218a848afdaeab1685c1c792c'
-api_secret = '721c122338c12d79106f1ba8196ea5ae'
+api_key = 'REPLACE_YOUR_KEY'
+api_secret = 'REPLACE_YOUR_SECRET'
 mailjet = Client(auth=(api_key, api_secret), version='v3.1')
 
+@app.route('/service')
+def service():
+    return 'Notification Service',200
 
 @app.route('/sendEmail',methods=["POST"])
 def sendEmail():
@@ -21,8 +23,8 @@ def sendEmail():
   'Messages': [
     {
       "From": {
-        "Email": "ana.customer1000@gmail.com",
-        "Name": "Cipher2022"
+        "Email": "REPLACE_YOUR_EMAIL",
+        "Name": "Ashish M J"
       },
       "To": [
         {
@@ -30,21 +32,17 @@ def sendEmail():
           "Name": name
         }
       ],
-      "Subject": "Greetings from Cipher2022.",
-      "TextPart": "My first Mailjet email",
-      "HTMLPart": "<h3>Dear {{name}} , Your registration for Cipher2022 is confirmed.<\br>Thank you.",
+      "Subject": "Greetings from Ashish M J",
+      "TextPart": "Welcome to the Tutorial",
+      "HTMLPart": f"<h3>Dear {name} , Hope you have understood the basics of Kubernetes.Thanks for reading!",
       "CustomID": "AppGettingStartedTest"
     }
   ]
 }
-        result = mailjet.send.create(data=data)
-        print(result.status_code)
-        print (result.json())
+        mailjet.send.create(data=data)
         return jsonify({"Accepted":202}),202
     else:
         return jsonify({"Bad Request":400}),400
         
-    
 if __name__ == '__main__':
 	app.run(debug=True)
-
